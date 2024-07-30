1972b520852abc7ac53ecec8404eede8d5ab77bd
axel <axel.aatl@gmail.com>
2024-07-30 00:27:16 -0600

    [ADD] API

---
+++ src/Entity/Category.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[ApiResource(
+    // shortName: 'nuevo-acceso',
+    // routePrefix: 'prefix',
+    description: 'Información adicional sobre esta clase',
+    operations: [
+        new Get(),
+        new GetCollection(),
+    ],
+    normalizationContext: ['groups' => ['category:read']], // GET
+)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['category:read', 'post:read'])]
+    /**
+     * Identificador único en la tabla category
+     */
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['category:read', 'post:read'])]
+    /**
+     * Nombre de la categoria
+     */
+    private ?string $name = null;
+
+    /**
+     * @var Collection<int, Post>
+     */
+    #[ORM\OneToMany(targetEntity: Post::class, mappedBy: 'category', orphanRemoval: true)]
+    #[Groups(['category:read'])]
+    /**
+     * Publicaciones que pertenecen a la categoria, relación OneToMany
+     */
+    private Collection $posts;
+
+    public function __construct()
+    {
+        $this->posts = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Post>
+     */
+    public function getPosts(): Collection
+    {
+        return $this->posts;
+    }
+
+    public function addPost(Post $post): static
+    {
+        if (!$this->posts->contains($post)) {
+            $this->posts->add($post);
+            $post->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removePost(Post $post): static
+    {
+        if ($this->posts->removeElement($post)) {
+            // set the owning side to null (unless already changed)
+            if ($post->getCategory() === $this) {
+                $post->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Post.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post as Store;
+use App\Repository\PostRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+#[ApiResource(
+    description: 'Entidad de las publicaciones con title, body y category',
+    operations: [
+        new Get(
+            normalizationContext: ['groups' => ['post:read', 'post: read:item']], // GET
+        ),
+        new GetCollection(
+            normalizationContext: ['groups' => ['post:read', 'post:read:collection']], // GET
+        ),
+        new Patch(),
+        new Store(),
+    ],
+    // normalizationContext: ['groups' => ['read']], // GET
+    denormalizationContext: ['groups' => ['post:write']], // POST, PUT, PATCH
+    paginationItemsPerPage: 8,
+)]
+#[ApiFilter(SearchFilter::class, properties: [
+    'title' => 'partial', // exact, partial, start, end, word_start
+    'body' => 'partial',
+    'category.name' => 'partial',
+])]
+#[ApiFilter(OrderFilter::class, properties: ['id'])]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['post:read'])]
+    /**
+     * Campo ID, único y auto_increment
+     */
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['post:read', 'post:write'])]
+    #[Assert\NotBlank]
+    /**
+     * Título de una publicación, frase
+     */
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['post:read:item', 'post:write'])]
+    #[Assert\NotBlank]
+    /**
+     * Contenido de la publicación, 1200 palabras
+     */
+    private ?string $body = null;
+
+    #[ORM\ManyToOne(inversedBy: 'posts')]
+    #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['post:read', 'post:write'])]
+    #[Assert\NotBlank]
+    /**
+     * Campo de la categoría, relación ManyToOne
+     */
+    private ?Category $category = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    #[Groups(['post:read:collection'])]
+    /**
+     * Resumen de la publicación, su función es mostrarse en los listados
+     */
+    public function getSummary($len = 70): ?string
+    {
+        if (mb_strlen($this->body) <= $len) {
+            return $this->body;
+        }
+        return mb_substr($this->body, 0, 70).'[...]';
+    }
+
+
+    public function setBody(string $body): static
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): static
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
